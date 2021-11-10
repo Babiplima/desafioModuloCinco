fedd49a1627285c1c4ebd74696a7a5f94d97530d
Bárbara Ann Lima <babiplima@gmail.com>
2021-11-10 17:37:45 -0300

    Modelar Classe Service

--- src/main/java/zup/com/br/gerenciadorConta/exceptions/ContaService.java
+++ src/main/java/zup/com/br/gerenciadorConta/exceptions/ContaService.java
@@ -1,5 +1,12 @@
 package zup.com.br.gerenciadorConta.exceptions;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class ContaService {
+
+    @Autowired
+    private ContaRepository contaRepository;
+
+
 
 }
