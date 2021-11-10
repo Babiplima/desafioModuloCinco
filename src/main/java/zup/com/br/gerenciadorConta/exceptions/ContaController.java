ca036ba1dae85b30f98504ef81c35991d6a59b5a
Bárbara Ann Lima <babiplima@gmail.com>
2021-11-10 15:20:27 -0300

    Modelar Classe ContaController

--- src/main/java/zup/com/br/gerenciadorConta/exceptions/ContaController.java
+++ src/main/java/zup/com/br/gerenciadorConta/exceptions/ContaController.java
@@ -1,4 +1,15 @@
 package zup.com.br.gerenciadorConta.exceptions;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping ("/gereciadorCadastro")
 public class ContaController {
+    @Autowired
+    private ContaService cadastroService;
+    @Autowired
+    private ModelMapper modelMapper;
 }
