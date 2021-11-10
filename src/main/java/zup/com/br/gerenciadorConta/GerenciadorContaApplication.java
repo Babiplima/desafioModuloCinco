93fcd4dd0563128ac99fcc41655a88361f50c8d6
Bárbara Ann Lima <babiplima@gmail.com>
2021-11-10 14:25:35 -0300

    Criar projeto

---
+++ src/main/java/zup/com/br/gerenciadorConta/GerenciadorContaApplication.java
@@ -0,0 +1,13 @@
+package zup.com.br.gerenciadorConta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorContaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorContaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/zup/com/br/gerenciadorConta/GerenciadorContaApplicationTests.java
@@ -0,0 +1,13 @@
+package zup.com.br.gerenciadorConta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorContaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
