7d1444270cbbd879f857c664917d6c6db14adab4
Bárbara Ann Lima <babiplima@gmail.com>
2021-11-11 15:35:30 -0300

    Modelar Classe ContaSaidaDTO

--- src/main/java/zup/com/br/gerenciadorConta/cadastro/dto/ContaSaidaDTO.java
+++ src/main/java/zup/com/br/gerenciadorConta/cadastro/dto/ContaSaidaDTO.java
@@ -1,4 +1,27 @@
 package zup.com.br.gerenciadorConta.cadastro.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import zup.com.br.gerenciadorConta.Enum.Status;
+import zup.com.br.gerenciadorConta.Enum.Tipo;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class ContaSaidaDTO {
+    private int id;
+    private String nome;
+    private double valor;
+    private Tipo tipo;
+    private LocalDate dataDeVencimento;
+    private LocalDateTime dataDePagamento;
+    private Status status;
+
 }
