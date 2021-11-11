d2436b900b829dbaf12f7352ebc32674d4a650e6
Bárbara Ann Lima <babiplima@gmail.com>
2021-11-11 14:38:17 -0300

    Modelar Classe ExibirContaDTO

--- src/main/java/zup/com/br/gerenciadorConta/cadastro/dto/ExibirContaDTO.java
+++ src/main/java/zup/com/br/gerenciadorConta/cadastro/dto/ExibirContaDTO.java
@@ -1,4 +1,18 @@
 package zup.com.br.gerenciadorConta.cadastro.dto;
 
+import jdk.jfr.SettingDefinition;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import zup.com.br.gerenciadorConta.Enum.Status;
+
+@Getter @Setter
+@NoArgsConstructor @AllArgsConstructor
 public class ExibirContaDTO {
+
+    private int id;
+    private String name;
+    private double valor;
+    private Status status;
 }
