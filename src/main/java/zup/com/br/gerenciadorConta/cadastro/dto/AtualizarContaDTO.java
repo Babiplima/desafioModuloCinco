dd00fa9507c18ae62e1722531ea99ca53ebad77a
Bárbara Ann Lima <babiplima@gmail.com>
2021-11-11 14:35:01 -0300

    Criar Classe ExibirContaDTO

--- src/main/java/zup/com/br/gerenciadorConta/cadastro/dto/AtualizarContaDTO.java
+++ src/main/java/zup/com/br/gerenciadorConta/cadastro/dto/AtualizarContaDTO.java
@@ -1,4 +1,15 @@
 package zup.com.br.gerenciadorConta.cadastro.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import zup.com.br.gerenciadorConta.Enum.Status;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class AtualizarContaDTO {
+    private Status status;
 }
