9b81608074a337ccecb696ffc9ecdae6db3e0a98
Bárbara Ann Lima <babiplima@gmail.com>
2021-11-17 11:35:25 -0300

    Criar metodo tratar exceção Id não encontrado

--- src/main/java/zup/com/br/gerenciadorConta/exceptions/ExceptionIdNaoEncontrado.java
+++ src/main/java/zup/com/br/gerenciadorConta/exceptions/ExceptionIdNaoEncontrado.java
@@ -1,4 +1,8 @@
 package zup.com.br.gerenciadorConta.exceptions;
 
 public class ExceptionIdNaoEncontrado {
+
+    public ExceptionIdNaoEncontrado (String mensagem){
+        super(mensagem);
+    }
 }
