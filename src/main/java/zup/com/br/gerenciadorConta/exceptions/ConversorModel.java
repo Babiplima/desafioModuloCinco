1f87236db13b51710ec3e6c64fa7f2c75f6d2d53
Bárbara Ann Lima <babiplima@gmail.com>
2021-11-17 11:24:40 -0300

    Modelar Classe Com Anotações Component e Bean

--- src/main/java/zup/com/br/gerenciadorConta/exceptions/ConversorModel.java
+++ src/main/java/zup/com/br/gerenciadorConta/exceptions/ConversorModel.java
@@ -1,9 +1,13 @@
 package zup.com.br.gerenciadorConta.exceptions;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
 
+@Component
 public class ConversorModel {
 
+    @Bean
     public ModelMapper modelMapper(){
         return new ModelMapper();
     }
